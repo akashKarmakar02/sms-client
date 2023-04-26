3a585e5be6612e62dd06f2730bd6b088bce21753
akashKarmakar02 <akash.anan03@gmail.com>
2023-04-26 20:45:58 +0530

    adding submit event to form

--- src/components/edit_student.tsx
+++ src/components/edit_student.tsx
@@ -56,7 +56,7 @@ function EditStudent() {
   }
 
   return (
-    <form className="p-12 text-left">
+    <form className="p-12 text-left" onSubmit={handleUpdate}>
       <div className="relative z-0 w-full mb-6 group">
         <input
           type="email"
@@ -191,7 +191,6 @@ function EditStudent() {
       {!isPending && (
         <button
           type="submit"
-          onClick={handleUpdate}
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Update
