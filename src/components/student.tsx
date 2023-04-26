fcd5e1c10c0b9c1aaffa85dd666a9fafa2dbb268
akashKarmakar02 <akash.anan03@gmail.com>
2023-04-26 21:54:29 +0530

    changing student card size

--- src/components/student.tsx
+++ src/components/student.tsx
@@ -1,6 +1,6 @@
 function Student(props: any) {
   return (
-    <div className="block max-w-5xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <div className="block max-w-4xl p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         Student Details
       </h5>
