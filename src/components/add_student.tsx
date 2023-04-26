6f588838e07965221879c8526663ab3dca2c7476
akashKarmakar02 <akash.anan03@gmail.com>
2023-04-26 20:44:14 +0530

    adding submit event to form

--- src/components/add_student.tsx
+++ src/components/add_student.tsx
@@ -37,7 +37,7 @@ function CreateStudentForm() {
   }
 
   return (
-    <form className="p-12 text-left">
+    <form className="p-12 text-left" onSubmit={handleSubmit}>
       <div className="relative z-0 w-full mb-6 group">
         <input
           type="email"
@@ -172,7 +172,6 @@ function CreateStudentForm() {
       {!isPending && (
         <button
           type="submit"
-          onClick={handleSubmit}
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Add Student
